01e7d57997e3eefa745d3abba970682139b01c77
reigota <reigota@gmail.com>
2015-09-29 20:51:15 -0300

    iniciando uma scene de demonstracao de uso do Core

---
+++ Assets/Core/SampleScene/Scripts/NavMeshAgentController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshAgentController : MonoBehaviour {
+
+
+    public Action OnReachDestination;
+
+    private NavMeshAgent m_navMeshAgent;
+    private int m_areaMask;
+    private bool m_reachDestination;
+
+    private void Awake()
+    {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+        m_areaMask = m_navMeshAgent.areaMask;
+    }
+
+
+    public void SetDestination(Vector3 destination)
+    {
+        NavMeshHit navMeshHit;
+
+        NavMesh.SamplePosition(destination, out navMeshHit, 2f, m_areaMask);
+
+        if (!navMeshHit.hit)
+            return;
+
+        m_reachDestination = false;
+
+        m_navMeshAgent.SetDestination(navMeshHit.position);
+
+        StopCoroutine(CheckDestination());
+        StartCoroutine(CheckDestination());
+    }
+
+    private IEnumerator CheckDestination()
+    {
+        while (!m_reachDestination)
+        {
+            if (!m_navMeshAgent.pathPending)
+            {
+                if (m_navMeshAgent.remainingDistance <= m_navMeshAgent.stoppingDistance)
+                {
+                    if (!m_navMeshAgent.hasPath || Math.Abs(m_navMeshAgent.velocity.sqrMagnitude) < float.Epsilon)
+                    {
+                        m_reachDestination = true;
+                        if (OnReachDestination != null)
+                            OnReachDestination();
+                    }
+                }
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ Assets/Core/SampleScene/Scripts/PawnBase.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(NavMeshAgentController))]
+public class PawnBase : MonoBehaviour {
+
+    private NavMeshAgentController m_navMeshAgentController;
+
+    private void Awake()
+    {
+        m_navMeshAgentController = GetComponent<NavMeshAgentController>();
+    }
+
+    private void Start()
+    {
+        OnReachDestination();
+    }
+
+    private void OnEnable()
+    {
+        m_navMeshAgentController.OnReachDestination += OnReachDestination;
+    }
+
+    private void OnDisable()
+    {
+        m_navMeshAgentController.OnReachDestination -= OnReachDestination;
+    }
+
+    private void OnReachDestination()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(0, 7), 0, Random.Range(0, 7));
+        m_navMeshAgentController.SetDestination(randomPosition);
+    }
+}
+ 
